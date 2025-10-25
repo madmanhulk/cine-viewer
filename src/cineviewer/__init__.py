c57f573ddd38204b03ed8a316dc18268bf1af575
ReidP <reidmpetro@gmail.com>
2025-10-25 17:17:31 -0400

    more reorg

--- src/cineviewer/__init__.py
+++ src/cineviewer/__init__.py
@@ -4,7 +4,7 @@ CineViewer - A web-based cinematography analysis tool
 import os
 
 # Read version from VERSION file
-with open(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'VERSION')) as f:
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
     __version__ = f.read().strip()
 
 from .app import app  # noqa: F401
