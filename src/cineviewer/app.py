cd2df015aa4390ac94c855210dacc9328f0d5267
ReidP <reidmpetro@gmail.com>
2025-10-26 13:31:49 -0400

    production ready w/ multi clients v0.26

--- src/cineviewer/app.py
+++ src/cineviewer/app.py
@@ -266,4 +266,5 @@ if __name__ == '__main__':
     cleanup_thread = threading.Thread(target=cleanup_clients, daemon=True)
     cleanup_thread.start()
     
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080, threads=6)
